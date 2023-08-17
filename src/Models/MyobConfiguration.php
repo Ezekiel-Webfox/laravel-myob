bc5ed8322458f694543a1dacbe6ada811eadc545
Ezekiel-Webfox <114041728+Ezekiel-Webfox@users.noreply.github.com>
2023-08-17 16:09:50 +1200

    Added hasConfiguredCompanyFile attribute to the myob configuration model

--- src/Models/MyobConfiguration.php
+++ src/Models/MyobConfiguration.php
@@ -3,6 +3,7 @@
 namespace Webfox\MYOB\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MyobConfiguration extends Model
@@ -11,6 +12,10 @@ class MyobConfiguration extends Model
 
     protected $casts = [
         'expires_at' => 'datetime',
+    ];
+
+    protected $appends = [
+        'has_configured_company_file',
     ];
 
     protected $hidden = [
@@ -24,5 +29,17 @@ class MyobConfiguration extends Model
         static::saving(function (MyobConfiguration $model) {
             $model->expires_at = now()->addSeconds(900);
         });
+    }
+
+    public function hasConfiguredCompanyFile(): Attribute
+    {
+        return Attribute::make(
+            get: function ($val, $attributes) {
+                return $attributes['company_file_id']
+                    && $attributes['company_file_token']
+                    && $attributes['company_file_name']
+                    && $attributes['company_file_uri'];
+            }
+        );
     }
 }
